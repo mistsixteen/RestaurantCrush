d402049357fb55e7ec0168d46761c2d8f2312904
mistsixteen <mist.sixteen@gmail.com>
2022-02-15 07:23:14 +0900

    기본 노드 이동 인식기능 구현, 보드 생성용 테스트 프리펩 생성
    기본 노드 이동 인식기능 구현, 보드 생성용 테스트 프리펩 생성

---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class NodeScript : MonoBehaviour
+{
+    [SerializeField]
+    private float moveLength;
+
+    private bool isClicked;
+
+    private Vector3 mousePoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isClicked)
+        {
+            Vector3 mousePoint2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(mousePoint2.y - mousePoint.y > moveLength)
+            {
+                Debug.Log("UP");
+                isClicked = false;
+            }
+            else if(mousePoint.y - mousePoint2.y > moveLength)
+            {
+                Debug.Log("DOWN");
+                isClicked = false;
+            }
+            else if (mousePoint2.x - mousePoint.x > moveLength)
+            {
+                Debug.Log("R");
+                isClicked = false;
+            }
+            else if (mousePoint.x - mousePoint2.x > moveLength)
+            {
+                Debug.Log("L");
+                isClicked = false;
+            }
+
+        }
+    }
+    //IPointerDownHandler
+    public void OnMouseDown()
+    {
+        //toDo : NodeBoard에 isClicked Check 요청
+        isClicked = true;
+        mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    
+    //IPointerUpHandler
+    public void OnMouseUp()
+    {
+        //toDo : NodeBoard에 isClicked 종료 요청
+        isClicked = false;
+    }
+    // On Pointer Up
+}
