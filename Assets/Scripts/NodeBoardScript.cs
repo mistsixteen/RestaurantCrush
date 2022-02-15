ec8fe89fd65c6898bc587ecc2c16d024c5b9351d
mistsixteen <mist.sixteen@gmail.com>
2022-02-15 09:45:14 +0900

    NodeBoard 생성, 보드에 맞춰 Node들 생성하도록 수정(임시 프리팹)
    NodeBoard 생성, 보드에 맞춰 Node들 생성하도록 수정(임시 프리팹)

---
+++ Assets/Scripts/NodeBoardScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeBoardScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject tempNode;
+    public float baseXPos;
+    public float baseYPos;
+
+    public float NodeXDistance;
+    public float NodeYDistance;
+
+
+    GameObject[,] NodeBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NodeBoard = new GameObject[7, 7];
+        for(int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                NodeBoard[i,j] = Instantiate(tempNode);
+                Transform rect = NodeBoard[i, j].GetComponent<Transform>();
+                rect.position = new Vector3(baseXPos + (float)i * NodeXDistance, baseYPos + (float)j * NodeYDistance, 0.0f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
