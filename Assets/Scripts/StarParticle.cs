5f0e33ea8c103d28600b103e3f19df3ea094d1de
mistsixteen <mist.sixteen@gmail.com>
2022-03-13 20:46:34 +0900

    StarParticle에 코루틴 적용
    FixedUpdate -> Coroutine으로 변경

--- Assets/Scripts/StarParticle.cs
+++ Assets/Scripts/StarParticle.cs
@@ -7,29 +7,43 @@ public class StarParticle : MonoBehaviour
     // Start is called before the first frame update
 
     public Vector3 MoveVector;
-    public int FrameLeft = 3000;
+    public int FrameLeft = 20;
 
-    public float moveSpeed = 0.005f;
-    public float gValue = 0.001f;
+    public float moveSpeed = 0.05f;
+    public float gValue = 0.01f;
+
+    private SpriteRenderer Renderer;
+    private Coroutine particleCorutine;
 
     void Start()
     {
+        Renderer = this.GetComponent<SpriteRenderer>();
 
+        particleCorutine = StartCoroutine(ParticleFadeOut());
     }
 
-    void FixedUpdate()
-    {
-        FrameLeft--;
-        transform.position += MoveVector * moveSpeed;
-        MoveVector.y -= gValue;
-
-        if (FrameLeft <= 0)
-            Destroy(this.gameObject);
-    }
 
     public void SetMoveVector(Vector2 mov)
     {
         MoveVector = new Vector3(mov.x, mov.y, 0.0f);
+    }
+
+    IEnumerator ParticleFadeOut()
+    {
+        Color color = Renderer.color;
+        while(color.a > 0.0f)
+        {
+            FrameLeft--;
+            transform.position += MoveVector * moveSpeed;
+            MoveVector.y -= gValue;
+            color.a -= 0.05f;
+            Renderer.color = color;
+            if (FrameLeft <= 0)
+                Destroy(this.gameObject);
+
+            yield return new WaitForSeconds(0.05f);
+        }
+        
     }
 
 }
